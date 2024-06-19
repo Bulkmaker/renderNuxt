84e5c2f2cad45b43776b7c380a547b73fc5c3b19
misha <ibull37@gmail.com>
2024-06-19 23:15:32 +0200

    Create error.vue

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col items-center">
+        <div class="text-indigo-500 font-bold text-7xl">
+            404
+        </div>
+
+        <div class="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+            Страница не существует
+        </div>
+
+        <div class="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
+            Страница, которую вы ищете, не существует
+        </div>
+    </div>
+</template>
